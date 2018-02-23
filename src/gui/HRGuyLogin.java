c4ea9a4627e133679f10cfaa06e2d4a1a91691c5
jero77 <34156935+jero77@users.noreply.github.com>
2018-02-23 20:08:28 +0100

    Minor changes

--- src/gui/HRGuyLogin.java
+++ src/gui/HRGuyLogin.java
@@ -26,6 +26,8 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
 public class HRGuyLogin extends JFrame {
 
@@ -138,7 +140,7 @@ public class HRGuyLogin extends JFrame {
 					login(user, password);
 					
 					//Successful login
-					HRGuyGUI gui = new HRGuyGUI(dbc);
+					HRGuyGUI gui = new HRGuyGUI(dbc, user);
 					gui.setVisible(true);
 					dispose();
 					
