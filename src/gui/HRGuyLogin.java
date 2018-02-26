750f85a12bdbdb575fe9b122e1be6449aabf62d4
jero77 <34156935+jero77@users.noreply.github.com>
2018-02-26 17:47:35 +0100

    Completed Guy Login

--- src/gui/HRGuyLogin.java
+++ src/gui/HRGuyLogin.java
@@ -2,34 +2,40 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import javax.security.sasl.AuthenticationException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 import javax.swing.border.EmptyBorder;
 
 import dbc.DatabaseController;
 import dbc.PasswordHash;
 
-import javax.swing.JLabel;
-
-import java.awt.Font;
-
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JButton;
-
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-
+/**
+ * This class provides a short frame where an HR Guy can login 
+ * to the HRGuyGUI with his credentials (id and password).
+ * @author Jero
+ *
+ */
 public class HRGuyLogin extends JFrame {
+
+	//auto generated
+	private static final long serialVersionUID = -2555735939261880148L;
+
 
 	private JPanel contentPane;
 	
