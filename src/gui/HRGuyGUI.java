3727349fb16b33bb9ba29fd01294ae1199103a6e
jero77 <34156935+jero77@users.noreply.github.com>
2018-02-27 20:40:31 +0100

    Removed an Import

--- src/gui/HRGuyGUI.java
+++ src/gui/HRGuyGUI.java
@@ -7,7 +7,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.net.URL;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
